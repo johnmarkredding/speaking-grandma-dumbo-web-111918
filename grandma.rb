0dec245e0dcf0be414c713ce4ad4e4dafe8fe483
John Mark Redding <johnmarkredding@gmail.com>
2018-11-07 15:28:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 
 def speak_to_grandma(message)
   if message == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
+    'I LOVE YOU TOO PUMPKIN!'
   elsif message == message.upcase
-    puts 'NO, NOT SINCE 1938!'
+    'NO, NOT SINCE 1938!'
   else
-    puts 'HUH?! SPEAK UP, SONNY!'
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
 
